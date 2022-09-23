2af1d5e5f1b029a4d2e5d8c4ba5de2782697758b
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-09-23 13:53:56 +0200

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { UserComponent } from "./user/user.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
-
+const routes:Routes=[
+  {path:"signup",component:SignUpComponent},
+  {path:"user",component:UserComponent},
+  {path:"",component:UserProfileComponent},
+  {path:"menu",component:MenuComponent},
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    SignUpComponent,
+    MenuComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
